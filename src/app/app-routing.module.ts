2eab3b0d1d6405705a39245f92aed020334c3eba
Shahriar Hussain <shahriarhus@gmail.com>
2024-12-15 17:49:28 +0530

    Generated components
    About, Solutions, Contact & added to routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SolutionsComponent } from './solutions/solutions.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path:'navbar', component: NavbarComponent},
-  { path: 'home', component: HomeComponent },
+  {path: 'home', component: HomeComponent },
+  {path: 'about', component: AboutComponent},
+  {path: 'solutions', component: SolutionsComponent},
+  {path: 'contact', component: ContactComponent},
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SolutionsComponent } from './solutions/solutions.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    SolutionsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
