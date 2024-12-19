e104caedd393f9d7ad0bee83241b3002f4557102
Shahriar Hussain <shahriarhus@gmail.com>
2024-12-19 14:19:35 +0530

    Trying to add animations

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,17 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AppComponent } from './app/app.component';
+
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true
 })
   .catch(err => console.error(err));
+
+  bootstrapApplication(AppComponent, {
+    providers: [
+      provideAnimationsAsync(),
+    ]
+  });
