7e10b7549fa742408963ea790060f64cdd250b14
ymohd022 <ymohd022@gmail.com>
2024-12-31 17:36:23 +0530

    added portfolio container in home page and partners logo added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,36 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { HomeService } from '../services/home.service';
+import { Component } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('slideAnimation', [
+      transition(':increment', [
+        style({ transform: 'translateX(100%)', opacity: 0 }),
+        animate('500ms ease-out', style({ transform: 'translateX(0)', opacity: 1 }))
+      ]),
+      transition(':decrement', [
+        style({ transform: 'translateX(-100%)', opacity: 0 }),
+        animate('500ms ease-out', style({ transform: 'translateX(0)', opacity: 1 }))
+      ])
+    ])
+  ]
 })
-export class HomeComponent implements OnInit {
-  marketingCards: any[] = [];
-  currentContent: string = 'digitalMarketing'; 
+export class HomeComponent {
+  currentIndex = 0;
+  currentContent: string = 'digitalMarketing';
+  portfolioProjects = [
+    {
+      title: 'Skill Development HUB',
+      description: 'Leading news website of India with over 10000+ visitors everyday.',
+      image: 'sdhub.png',
+      link: 'https://sdhub.in/',
+    },
+    {
+      title: 'Professional Summit',
+      description: 'Advanced healthcare management system for patient records.',
+      image: 'PSF-Summit.png',
+      link: 'https://professionalssummit.com/',
+    },
+    {
+      title: 'Luminate Web Solutions',
+      description: 'Comprehensive learning management system with interactive courses.',
+      image: './Luminate Web Solutions.svg',
+      link: 'https://luminatewebsol.com/',
+    },
+   
+  ];
 
-  constructor (
-    private HomeService: HomeService
-  ) {}
-
-  ngOnInit(): void {
-    this.getMcards();
+  next() {
+    this.currentIndex = (this.currentIndex + 1) % this.portfolioProjects.length;
   }
 
-  getMcards(): void {
-    this.HomeService.getMarketingCards().subscribe({
-      next: (response) => {
-        console.log(response);
-        this.marketingCards = response;
-        console.log(this.marketingCards)
-      },
-      error(err) {
-        console.log(err)
-      },
-    })
+  prev() {
+    this.currentIndex = this.currentIndex === 0 ? 
+      this.portfolioProjects.length - 1 : this.currentIndex - 1;
   }
-
   showContent(contentType: string) {
     this.currentContent = contentType;
   }
